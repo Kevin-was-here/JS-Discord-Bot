797f4dbe05a17604337f9909869974e19484e30a
Kevin_was_here <xuyang4@ualberta.ca>
2022-07-06 17:17:52 -0600

    remove some stuffs

--- musictest.js
+++ musictest.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const { prefix, token } = require("./config.json");
 const ytdl = require("ytdl-core");
 
-const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Discord.Client();
 
 const queue = new Map();
 
